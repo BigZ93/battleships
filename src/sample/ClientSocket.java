097b98ab9b6525237d42ca25c93255441f2cdd28
Michał <michal.t3@op.pl>
2019-05-10 11:35:02 +0200

    version 0.04

---
+++ src/sample/ClientSocket.java
@@ -0,0 +1,34 @@
+package sample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientSocket {
+    private Socket socket;
+    private Scanner scanner;
+    private ClientSocket(InetAddress serverAddress, int serverPort) throws Exception {
+        this.socket = new Socket(serverAddress, serverPort);
+        this.scanner = new Scanner(System.in);
+    }
+    private void start() throws IOException {
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
+            out.println(input);
+            out.flush();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        ClientSocket client = new ClientSocket(
+                InetAddress.getByName(args[0]),
+                Integer.parseInt(args[1]));
+
+        System.out.println("\r\nConnected to Server: " + client.socket.getInetAddress());
+        client.start();
+    }
+}
